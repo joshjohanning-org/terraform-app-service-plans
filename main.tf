e9ce1d75a016fa73d68557571e55724e33da10be
Josh Johanning <joshjohanning@github.com>
2023-03-22 15:33:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ provider "azurerm" {
   features {}
 }
 
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+
+  min = 10000
+  max = 99999
+}
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
