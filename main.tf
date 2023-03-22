d91f194f33a46ca0e532f73b042cc7f9eb3df01d
Josh Johanning <joshjohanning@github.com>
2023-03-22 15:40:26 -0500

    test

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "random_integer" "ri" {
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = "myResourceGroup-${random_integer.ri.result}"
-  location = "eastus"
+  location = "eastus4"
 }
 
 # Create the Linux App Service Plan
@@ -33,7 +33,7 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "QQQ2"
 }
 
 # Create the web app, pass in the App Service Plan ID
@@ -46,7 +46,7 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.appserviceplan[count.index].id
   https_only          = true
   site_config {
-    minimum_tls_version = "1.2"
+    minimum_tls_version = "1.44"
   }
 }
 
@@ -59,4 +59,5 @@ resource "azurerm_app_service_source_control" "sourcecontrol" {
   branch                 = "master"
   use_manual_integration = true
   use_mercurial          = false
+  fake_flag              = "yes"
 }
